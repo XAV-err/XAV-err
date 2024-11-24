411fa0039006c44540285cda80e17f6baf76075c
VFrogale <56niceman@gmail.com>
2024-11-24 02:33:50 -0500

    This is even more awesome

---
+++ ILoveMen.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ILoveMen {
+   
+   public static void main(String[] args){
+
+      Scanner input = new Scanner(System.in, "US-ASCII");
+      System.out.print("Please enter a number: ");
+      int number = input.nextInt();
+      number *= 69;
+      System.out.println("Damn your number just got multiplied by 69");
+      System.out.println(number);
+
+   }
+}
